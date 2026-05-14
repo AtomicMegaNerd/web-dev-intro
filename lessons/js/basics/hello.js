d3c9c51eaa63f0218107a2865feca0e56c454e2c
Chris Dunphy <chris.dunphy@fglsports.com>
2026-05-14 11:55:42 -0600

    Ran prettier

--- lessons/js/basics/hello.js
+++ lessons/js/basics/hello.js
@@ -1,25 +1,24 @@
-const greeting = "Hello World!"
+const greeting = "Hello World!";
 
 const firstName = "Chris";
 const lastName = "Dunphy";
 
-console.log(greeting)
+console.log(greeting);
 
 // String interpolation, string concat works but why do that like an animal?
 const sentence = `Hello ${firstName} ${lastName}! How are you!?`;
 
-console.log(sentence)
+console.log(sentence);
 
 // Define some variables
-let x = 9
-let y = 7
+let x = 9;
+let y = 7;
 
 if (x !== y) {
-  console.log(`${x} is not equal to ${y}.`)
+  console.log(`${x} is not equal to ${y}.`);
 } else {
-  console.log(`${x} is equal to ${y}.`)
+  console.log(`${x} is equal to ${y}.`);
 }
-
 
 // Back-ticks can be multi-line
 const longStr = `
@@ -30,6 +29,6 @@ import "fmt"
 func main() {
   fmt.Println("Hello, world")
 }
-`
+`;
 
-console.log(longStr)
+console.log(longStr);
