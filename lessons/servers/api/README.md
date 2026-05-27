b15dab2df9652356b1484b31cdfac13a8be9276f
Chris Dunphy <chris.dunphy@fglsports.com>
2026-05-27 15:25:04 -0600

    Query a public api

---
+++ lessons/servers/api/README.md
@@ -0,0 +1,9 @@
+# Making API requests from JS
+
+## List of Public API's
+
+[public-apis](https://github.com/toddmotto/public-apis)
+
+## Promise - MDN
+
+[Promise](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise)
---
+++ lessons/servers/api/js/dogs.js
@@ -0,0 +1,29 @@
+const DOG_URL = "https://dog.ceo/api/breeds/image/random";
+
+const dogTarget = document.querySelector(".dog-target");
+
+// Store the previous node
+let prevNode = undefined;
+
+const doFetch = () => {
+  const promise = fetch(DOG_URL);
+  // This is promise chaining...
+  promise
+    .then((response) => response.json())
+    .then((dog) => {
+      const img = document.createElement("img");
+      img.src = dog.message;
+      img.alt = "A random dog";
+      img.className = "dog-img";
+
+      if (prevNode === undefined) {
+        dogTarget.appendChild(img);
+      } else {
+        prevNode.replaceWith(img);
+      }
+      prevNode = img;
+      console.log(`number of children: ${dogTarget.childNodes.length}`);
+    });
+};
+
+document.querySelector(".dog-btn").addEventListener("click", doFetch);
