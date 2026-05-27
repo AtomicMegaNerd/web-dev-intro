b591ee681d786c58c69376e48f90a5ac89d766cc
Chris Dunphy <chris.dunphy@fglsports.com>
2026-05-27 15:31:36 -0600

    Add some comments

--- lessons/servers/api/js/dogs.js
+++ lessons/servers/api/js/dogs.js
@@ -1,10 +1,12 @@
 const DOG_URL = "https://dog.ceo/api/breeds/image/random";
 
+// This is the container we will insert the dog image node into
 const dogTarget = document.querySelector(".dog-target");
 
 // Store the previous node
 let prevNode = undefined;
 
+// This fetches a dog from the API and inserts the img into the DOM
 const doFetch = () => {
   const promise = fetch(DOG_URL);
   // This is promise chaining...
