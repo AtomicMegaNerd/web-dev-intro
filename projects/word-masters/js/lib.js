32a6b9033f5dcf9971da05617722eb570e3fe88d
Chris Dunphy <chris.dunphy@fglsports.com>
2026-06-10 16:35:13 -0600

    Save the work so far

--- projects/word-masters/js/lib.js
+++ projects/word-masters/js/lib.js
@@ -1,55 +1,83 @@
-const getWordURL = "https://words.dev-apis.com/word-of-the-day?random=1";
-const validateWordURL = "https://words.dev-apis.com/validate-word";
+// oxlint-disable max-statements
+// const getWordURL = "https://words.dev-apis.com/word-of-the-day?random=1";
+// const validateWordURL = "https://words.dev-apis.com/validate-word";
+//
+// const fetchWord = async () => {
+//   try {
+//     const resp = await fetch(getWordURL);
+//     if (resp.ok) {
+//       const respJson = await resp.json();
+//       return respJson.word;
+//     } else {
+//       throw new Error(`${resp.status} ${resp.statusText}`);
+//     }
+//   } catch (error) {
+//     console.log(`error calling our api ${error.message}`);
+//   }
+// };
+//
+// const validateWord = async (word) => {
+//   try {
+//     const wordObj = {
+//       word,
+//     };
+//     const resp = await fetch(validateWordURL, {
+//       method: "POST",
+//       body: JSON.stringify(wordObj),
+//     });
+//     if (resp.ok) {
+//       const respJson = await resp.json();
+//       return respJson.validWord;
+//     } else {
+//       throw new Error(`${resp.status} ${resp.statusText}`);
+//     }
+//   } catch (error) {
+//     console.log(`error calling our api ${error.message}`);
+//   }
+// };
 
-const fetchWord = async () => {
-  try {
-    const resp = await fetch(getWordURL);
-    if (resp.ok) {
-      const respJson = await resp.json();
-      return respJson.word;
-    } else {
-      throw new Error(`${resp.status} ${resp.statusText}`);
-    }
-  } catch (error) {
-    console.log(`error calling our api ${error.message}`);
-  }
-};
-
-const validateWord = async (word) => {
-  try {
-    const wordObj = {
-      word,
-    };
-    const resp = await fetch(validateWordURL, {
-      method: "POST",
-      body: JSON.stringify(wordObj),
-    });
-    if (resp.ok) {
-      const respJson = await resp.json();
-      return respJson.validWord;
-    } else {
-      throw new Error(`${resp.status} ${resp.statusText}`);
-    }
-  } catch (error) {
-    console.log(`error calling our api ${error.message}`);
-  }
-};
-
+// oxlint-disable-next-line no-unused-vars
 const isLetter = (ch) => /^[a-zA-Z]$/.test(ch);
 
-// This is how you wrap your async/await at the top level of a file
+const gameState = {
+  word: "",
+  currCell: 0,
+  rowBuffer: "",
+};
+
+const getKeyFromCurrentCell = async (cell) => {
+  cell.addEventListener("keydown", (event) => event);
+};
+
+const clearListenerFromCell = (cell) => {
+  cell.replaceWith(cell.cloneNode(true));
+};
+
+console.log("ready to go...");
+
 (async () => {
-  const randomWord = await fetchWord();
-  const words = ["trees", randomWord, "roflsaucenotaword"];
-  for (const word of words) {
+  while (true) {
+    const cell = document.querySelector(`.game-cell-${gameState.currCell}`);
+    console.log(`current cell is .game-cell-${gameState.currCell}`);
     // oxlint-disable-next-line no-await-in-loop
-    const isValid = await validateWord(word);
-    if (isValid) {
-      console.log(`${word} is valid`);
+    const event = await getKeyFromCurrentCell(cell);
+
+    if (event instanceof KeyboardEvent) {
+      switch (event.key) {
+        case "Backspace": {
+          cell.textContent = "";
+          clearListenerFromCell(cell);
+          gameState.currCell--;
+        }
+        default: {
+          if (!isLetter(event.key)) {
+            event.preventDefault();
+          } else {
+            cell.textContent = event.key;
+          }
+          gameState.currCell++;
+        }
+      }
     }
   }
-
-  console.log(`is 'a' a letter? ${isLetter("a")}`);
-  console.log(`is 'Z' a letter? ${isLetter("Z")}`);
-  console.log(`is '1' a letter? ${isLetter("1")}`);
 })();
